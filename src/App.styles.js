f303709c51a88afd1dd13a7308e6bcb80f1b42eb
Kacper Duniewicz <kacper.duniewicz@gmail.com>
2021-01-17 23:42:27 +0100

    Revert "tests for application, small fix to styles"

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core"
 const useStyles = makeStyles(() => ({
     root: {
         minHeight: "100vh",
-        backgroundSize: "cover",
+        backgroundSize: "fill",
         backgroundImage: "url('https://www.click2houston.com/resizer/wwm4WvUzr5b-DkNuL9Mh9GeruZg=/1600x1067/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/CAG6NQ7LSNFVXKUA27WHRGUHWM.jpg')",
     },
 }))
--- src/__test__/functions.test.js
+++
@@ -1,7 +0,0 @@
-import { battleResult } from "../functions/functions"
-import "@testing-library/jest-dom"
-
-test("Setting score", () => {
-    expect(battleResult({"firstCard": 2, "secondCard": 2}, [{"cost_in_credits": "100"}, {"cost_in_credits": "0"}]))
-        .toStrictEqual({newScore: {"firstCard": 3, "secondCard": 2}, battleResult: "Left"})
-})
